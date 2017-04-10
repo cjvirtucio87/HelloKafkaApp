207304adfdd4806e38ea3311674c86209b5caa82
Virtucio <VirtucioC@pragmatics.com>
2017-04-10 16:06:53 -0400

    Initial commit

---
+++ src/main/java/Producer.java
@@ -0,0 +1,25 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * Created by VirtucioC on 4/10/2017.
+ */
+public class Producer {
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9090");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+        for (int i = 0; i < 100; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("some-new-topic", "value-" + i);
+            producer.send(record);
+            Thread.sleep(250);
+        }
+
+        producer.close();
+    }
+}
---
+++ src/main/java/SparkConsumer.java
@@ -0,0 +1,66 @@
+
+import kafka.serializer.StringDecoder;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaPairInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka.KafkaUtils;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by VirtucioC on 4/10/2017.
+ */
+
+public class SparkConsumer {
+    public static Logger log = Logger.getLogger(SparkConsumer.class);
+
+    public static void main(String[] args) throws Exception {
+        SparkConf conf = new SparkConf()
+                .setAppName("hello-kafka")
+                .setMaster("local[*]");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaStreamingContext ssc = new JavaStreamingContext(sc, new Duration(2000));
+
+        JavaPairInputDStream dStream = SparkConsumer.createDStream(ssc);
+        SparkConsumer.process(dStream);
+
+        ssc.start();
+        ssc.awaitTermination();
+    }
+
+    private static  JavaPairInputDStream createDStream(JavaStreamingContext ssc) {
+        Map<String, String> kafkaParams = new HashMap<String, String>();
+        kafkaParams.put("metadata.broker.list", "localhost:9090");
+        Set<String> topics = Collections.singleton("some-new-topic");
+
+        return KafkaUtils.createDirectStream(
+                ssc,
+                String.class,
+                String.class,
+                StringDecoder.class,
+                StringDecoder.class,
+                kafkaParams,
+                topics
+        );
+    }
+
+    private static void process(JavaPairInputDStream dStream) {
+        VoidFunction<?> onEach = new VoidFunction<JavaPairRDD<String, String>>() {
+            public void call(JavaPairRDD<String, String> rdd) throws Exception {
+                System.out.println("--- New RDD with " + rdd.partitions().size() + " partitions and " + rdd.count() + " records ");
+            }
+        };
+
+        dStream.foreachRDD(onEach);
+    }
+
+}
